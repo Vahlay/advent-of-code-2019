b6e2c8275544d55ed401556d71b2efea7c72b3cf
Vahic Laydier <vahic@laydier.fr>
2019-12-01 14:13:18 +0100

    Formalize how to run scripts & commit naming

--- 1/part1.mjs
+++ 1/part1.mjs
@@ -6,7 +6,7 @@ function get_module_needed_fuel(mass){
     return Math.floor(mass/3) - 2;
 }
 
-let fuelNeeded = Fs.readFileSync('./input','utf-8')
+let fuelNeeded = Fs.readFileSync('./1/input','utf-8')
                     .split(Os.EOL)
                     .map(numAsStr => parseInt(numAsStr)) //Directly passing parseInt to map() causes some of the number to be parsed as NaN
                     .map(get_module_needed_fuel)
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Advent of Code 2019
+
+https://adventofcode.com/2019
+
+Implemented in NodeJS.
+
+All scripts are meant to be run from the root folder
+```
+node 1/part1.mjs
+```
+
+Commits that make progress on the resolution of a problem should start by `DxPy`  where x is the day and y the part of the problem, exemple :
+``` 
+D1P1 - Calculate fuel needed for modules
+```
