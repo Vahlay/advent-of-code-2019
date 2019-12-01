76e93f863254e6a5150cfdbb73a47b581c35aeb7
Vahic Laydier <vahic@laydier.fr>
2019-12-01 14:45:08 +0100

    D1P2 - Calculate the whole fuel

--- 1/part2.mjs
+++ 1/part2.mjs
@@ -6,11 +6,21 @@ function get_needed_fuel(mass){
     return Math.floor(mass/3) - 2;
 }
 
-const modulesFuel = Fs.readFileSync('./1/input','utf-8')
+/**
+ * Recursively add all the fuel needed to carry a certain mass of fuel
+ * @param fuelMass 
+ */
+function add_fuel_for_fuel(fuelMass){
+    return get_needed_fuel(fuelMass) > 0 ?
+        fuelMass + add_fuel_for_fuel(get_needed_fuel(fuelMass)) :
+        fuelMass;
+}
+
+const totalFuel = Fs.readFileSync('./1/input','utf-8')
                     .split(Os.EOL)
                     .map(numAsStr => parseInt(numAsStr)) //Directly passing parseInt to map() causes some of the number to be parsed as NaN
                     .map(get_needed_fuel)
+                    .map(add_fuel_for_fuel)
                     .reduce((totalFuel,moduleFuel) => totalFuel + moduleFuel);
 
-
-console.log(modulesFuel);
+console.log(totalFuel);
