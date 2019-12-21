edd3b95468ffce3506c626bb78d51df684b00617
Vahic Laydier <vahic@laydier.fr>
2019-12-21 18:33:42 +0100

    D2P2 - Minor stylistic fixes

--- 2/part2.mjs
+++ 2/part2.mjs
@@ -22,14 +22,12 @@ const program = Fs.readFileSync('./2/input','utf-8')
                 .map(numAsStr => parseInt(numAsStr)); //Directly passing parseInt to map() causes some of the number to be parsed as NaN
 
 const range = [...Array(100).keys()];
-/** @var {Array<Array<number>>} combinations */
 const combinations = range.map(noun => range.map(verb => [noun,verb])).flat();
 const expectedResult = 19690720;
 
 
-const expectedCombination = combinations.find((combination) => {
-    const [noun,verb] = combination;
+const expectedCombination = combinations.find(([noun,verb]) => {
     return run_program_with_noun_and_verb(program,noun,verb) === expectedResult;
-})
+});
 
 console.log("Day 2 Step 2 answer : ",expectedCombination[0]*100 + expectedCombination[1]);
